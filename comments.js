92da14784fabd69940d541082a5cb37647fb8767
Gabríel Sighvatsson <gabriel.sighvatsson@gmail.com>
2023-07-24 13:38:38 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,20 @@
+// Create a web server
+// Run: node comments.js
+// URL: http://localhost:3000/comments
+
+// Load the express module
+const express = require('express');
+
+// Create an express app
+const app = express();
+
+// Load the comments module
+const comments = require('./comments');
+
+// Use the comments module
+app.use('/comments', comments);
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
